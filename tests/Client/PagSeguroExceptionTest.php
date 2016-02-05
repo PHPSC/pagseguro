b8e901b7ef3ab3fdfbff8d8634e8bc51d6e7c7e0
Paulo Peixoto <rfoxtrot@gmail.com>
2016-02-05 13:20:28 -0300

    Testes Unitários para PagSeguroExceptions

--- tests/Client/PagSeguroExceptionTest.php
+++ tests/Client/PagSeguroExceptionTest.php
@@ -1,29 +1,26 @@
 <?php
 namespace PHPSC\PagSeguro\Client;
 
-use GuzzleHttp\Stream\Stream;
-use GuzzleHttp\Message\Response;
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Psr7\Response;
 
-class PagSeguroExceptionTest extends \PHPUnit_Framework_TestCase
-{
-    /**
-     * @test
-     */
-    public function createShouldCreateAGenericMessageWhenStatusCodeIsNot400()
-    {
-        $response = new Response(500, [], Stream::factory('Server gone mad'));
-        $exception = PagSeguroException::create($response);
+class PagSeguroExceptionTest extends \PHPUnit_Framework_TestCase {
+	/**
+	 * @test
+	 */
+	public function createShouldCreateAGenericMessageWhenStatusCodeIsNot400() {
+		$response = new Response(500, [], Psr7\stream_for('Server gone mad'));
+		$exception = PagSeguroException::create($response);
 
-        $this->assertInstanceOf(PagSeguroException::class, $exception);
-        $this->assertEquals('[500] A HTTP error has occurred: Server gone mad', $exception->getMessage());
-    }
+		$this->assertInstanceOf(PagSeguroException::class, $exception);
+		$this->assertEquals('[500] A HTTP error has occurred: Server gone mad', $exception->getMessage());
+	}
 
-    /**
-     * @test
-     */
-    public function createShouldParseAsXmlWhenStatusCodeIs400()
-    {
-        $xml = <<<'XML'
+	/**
+	 * @test
+	 */
+	public function createShouldParseAsXmlWhenStatusCodeIs400() {
+		$xml = <<<'XML'
 <?xml version="1.0" encoding="UTF-8"?>
 <errors>
     <error>
@@ -37,16 +34,16 @@ class PagSeguroExceptionTest extends \PHPUnit_Framework_TestCase
 </errors>
 XML;
 
-        $message = <<<'MESSAGE'
+		$message = <<<'MESSAGE'
 Some errors occurred:
 [11004] Currency is required.
 [11005] Currency invalid value: 100
 MESSAGE;
 
-        $response = new Response(400, [], Stream::factory($xml));
-        $exception = PagSeguroException::create($response);
+		$response = new Response(400, [], Psr7\stream_for($xml));
+		$exception = PagSeguroException::create($response);
 
-        $this->assertInstanceOf(PagSeguroException::class, $exception);
-        $this->assertEquals($message, $exception->getMessage());
-    }
+		$this->assertInstanceOf(PagSeguroException::class, $exception);
+		$this->assertEquals($message, $exception->getMessage());
+	}
 }
