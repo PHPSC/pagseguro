d17c413d24c0ed7e911cd15161b3649747843b0a
Luan Oliveira <luanecomp@yahoo.com.br>
2019-05-03 22:06:06 -0300

    aumentando token do pagseguro para 100 caracteres

--- src/Credentials.php
+++ src/Credentials.php
@@ -32,7 +32,7 @@ class Credentials
     public function __construct($email, $token, Environment $environment = null)
     {
         $this->email = substr($email, 0, 60);
-        $this->token = substr($token, 0, 32);
+        $this->token = substr($token, 0, 100);
         $this->environment = $environment ?: new Production();
     }
 
